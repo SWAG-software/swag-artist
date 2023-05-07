6da7f552cfe2b64178a428fa8a81b3db310986f9
JuancaJcA <avilafloresjc@gmail.com>
2023-05-06 22:00:34 -0400

    Project created

---
+++ src/main/kotlin/com/example/swagartist/SwagArtistApplication.kt
@@ -0,0 +1,11 @@
+package com.example.swagartist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwagArtistApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwagArtistApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/swagartist/SwagArtistApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.swagartist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwagArtistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
